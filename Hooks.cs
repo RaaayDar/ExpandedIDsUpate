db374dc2d758f2050b9441b9fd980701d2bb1f59
RaaayDar <154654181+RaaayDar@users.noreply.github.com>
2025-06-23 17:39:49 -0500

    Add files via upload

---
+++ Hooks.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using RWCustom;
+using BepInEx;
+using Debug = UnityEngine.Debug;
+
+
+#pragma warning disable CS0618
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+namespace Hooks;
+
+[BepInPlugin("RaaayDar.BiggerIDrange", "IDs Expanded UPDATED", "1.5.0")]
+public class Hooks : BaseUnityPlugin
+{
+    
+    private void OnEnable()
+    {
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInit;
+        MachineConnector.SetRegisteredOI("RaaayDar.BiggerIDrange", new Options());
+    }
+
+    private bool IsInit;
+    private void RainWorldOnOnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+        try
+        {
+            if (IsInit) return;
+
+            //Your hooks go here
+            On.RainWorldGame.GetNewID += RainWorldGame_GetNewID;
+            On.RainWorldGame.GetNewID_int += RainWorldGame_GetNewID_int;
+            
+
+            On.RainWorldGame.ShutDownProcess += RainWorldGameOnShutDownProcess;
+            On.GameSession.ctor += GameSessionOnctor;
+
+            IsInit = true;
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex);
+            throw;
+        }
+    }
+
+    private EntityID RainWorldGame_GetNewID_int(On.RainWorldGame.orig_GetNewID_int orig, RainWorldGame self, int spawner)
+    {
+        return new EntityID(spawner, UnityEngine.Random.Range(-2147483648, 2147483647));
+    }
+
+    private EntityID RainWorldGame_GetNewID(On.RainWorldGame.orig_GetNewID orig, RainWorldGame self)
+    {
+        return new EntityID(-1, UnityEngine.Random.Range(-2147483648, 2147483647));
+        
+    }
+
+    private void RainWorldGameOnShutDownProcess(On.RainWorldGame.orig_ShutDownProcess orig, RainWorldGame self)
+    {
+        orig(self);
+        ClearMemory();
+    }
+    private void GameSessionOnctor(On.GameSession.orig_ctor orig, GameSession self, RainWorldGame game)
+    {
+        orig(self, game);
+        ClearMemory();
+    }
+
+    #region Helper Methods
+
+    private void ClearMemory()
+    {
+        //If you have any collections (lists, dictionaries, etc.)
+        //Clear them here to prevent a memory leak
+        //YourList.Clear();
+    }
+
+
+    #endregion
+}
---
+++ SelectionMenuUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Menu;
+using Menu.Remix.MixedUI;
+using Menu.Remix.MixedUI.ValueTypes;
+using RWCustom;
+using UnityEngine;
+
+namespace ExpandedIDs
+{
+    internal class Options : OptionInterface
+    {
+        private readonly RainWorld rainWorld;
+
+       
+        public Options()
+        {
+            this.rainWorld = rainWorld;   
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            Tabs = [new OpTab(this)];
+
+            var title = new OpLabel(new Vector2(0f, 345f), new Vector2(600f, 30f), "SPAWN PARAMETERS", FLabelAlignment.Center, true)
+            {
+                verticalAlignment = OpLabel.LabelVAlignment.Center
+            };
+            title.label.shader = Custom.rainWorld.Shaders["MenuText"];
+
+            
+
+         
+        }
+    }
+}
